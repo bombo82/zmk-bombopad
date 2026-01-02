71e5a0dc9dab6bb40d3775a364458c9fc503ad29
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2026-01-02 18:42:00 +0100

    Add new widgets for Bombopad shield including images and status handling
    Signed-off-by: Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>

---
+++ boards/shields/bombopad/widgets/util.c
@@ -0,0 +1,84 @@
+/*
+ *
+ * Copyright (c) 2023 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <zephyr/kernel.h>
+#include "util.h"
+
+LV_IMG_DECLARE(bolt);
+
+void rotate_canvas(lv_obj_t *canvas) {
+}
+
+void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
+    lv_draw_rect_dsc_t rect_black_dsc;
+    init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
+    lv_draw_rect_dsc_t rect_white_dsc;
+    init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
+
+    canvas_draw_rect(canvas, 0, 2, 29, 12, &rect_white_dsc);
+    canvas_draw_rect(canvas, 1, 3, 27, 10, &rect_black_dsc);
+    canvas_draw_rect(canvas, 2, 4, (state->battery + 2) / 4, 8, &rect_white_dsc);
+    canvas_draw_rect(canvas, 30, 5, 3, 6, &rect_white_dsc);
+    canvas_draw_rect(canvas, 31, 6, 1, 4, &rect_black_dsc);
+
+    if (state->charging) {
+        lv_draw_img_dsc_t img_dsc;
+        lv_draw_img_dsc_init(&img_dsc);
+        canvas_draw_img(canvas, 9, -1, &bolt, &img_dsc);
+    }
+}
+
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
+                    lv_text_align_t align) {
+    lv_draw_label_dsc_init(label_dsc);
+    label_dsc->color = color;
+    label_dsc->font = font;
+    label_dsc->align = align;
+}
+
+void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color) {
+    lv_draw_rect_dsc_init(rect_dsc);
+    rect_dsc->bg_color = bg_color;
+}
+
+void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width) {
+    lv_draw_line_dsc_init(line_dsc);
+    line_dsc->color = color;
+    line_dsc->width = width;
+}
+
+void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width) {
+    lv_draw_arc_dsc_init(arc_dsc);
+    arc_dsc->color = color;
+    arc_dsc->width = width;
+}
+
+void canvas_draw_line(lv_obj_t *canvas, const lv_point_t points[], uint32_t point_cnt,
+                      lv_draw_line_dsc_t *draw_dsc) {
+    for (uint32_t i = 1; i < point_cnt; ++i) {
+        lv_canvas_draw_line(canvas, points + i - 1, 2, draw_dsc);
+    }
+}
+void canvas_draw_rect(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, lv_coord_t w, lv_coord_t h,
+                      lv_draw_rect_dsc_t *draw_dsc) {
+    lv_canvas_draw_rect(canvas, x, y, w, h, draw_dsc);
+}
+
+void canvas_draw_arc(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, lv_coord_t r,
+                     int32_t start_angle, int32_t end_angle, lv_draw_arc_dsc_t *draw_dsc) {
+    lv_canvas_draw_arc(canvas, x, y, r, start_angle, end_angle, draw_dsc);
+}
+
+void canvas_draw_text(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, lv_coord_t max_w,
+                      lv_draw_label_dsc_t *draw_dsc, const char *txt) {
+    lv_canvas_draw_text(canvas, x, y, max_w, draw_dsc, txt);
+}
+
+void canvas_draw_img(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, const lv_img_dsc_t *src,
+                     lv_draw_img_dsc_t *draw_dsc) {
+    lv_canvas_draw_img(canvas, x, y, (const void *)src, draw_dsc);
+}
